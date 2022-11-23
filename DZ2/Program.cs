e511059b9774d659e1caafd11f8d91391f53d83c
yasya <kalgexavva1970@icloud.com>
2022-11-23 12:41:13 +0300

    add new file

---
+++ DZ2/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. Напишите
+//программу, которая будет находить строку с наименьшей суммой элементов.
+void NumberRowMinSumElements(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow += array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+int[,] GetArray(int m,int n,int minValue,int maxValue)
+{
+    int [,] result=new int[m,n];
+    for (int i=0;i<m;i++)
+    {
+       for (int j=0;j<n;j++) 
+       {
+        result[i,j]=new Random().Next(minValue, maxValue +1);
+       }
+    }
+    return result;
+}
+void PrintArray(int [,] inArray)
+{
+    for (int i=0;i<inArray.GetLength(0);i++)
+    {
+        for (int j=0;j<inArray.GetLength(0);j++)
+        {
+            Console.Write($"{inArray[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во стобцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(rows, cols, 0, 100);
+PrintArray(array);
+
+Console.WriteLine();
+NumberRowMinSumElements(array);
