bbfcf151a64b6a590108802a227aa017f62a682c
yasya <kalgexavva1970@icloud.com>
2022-11-25 15:25:31 +0300

    add new file

---
+++ ex2/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+//которая заменяет строки на столбцы. В случае, если это
+//невозможно, программа должна вывести сообщение для
+//пользователя.
+int [,] ChangeArray(int[,] array)
+{
+    int [,] result=new int[array.GetLength(0),array.GetLength(1)];
+    for (int i=0;i<array.GetLength(0);i++)
+    {
+       for (int j=0;j<array.GetLength(1);j++) 
+       {
+        result[i,j]=array[j,i];
+       }
+    }
+return array;
+}
+int[,] GetArray(int m,int n,int minValue,int maxValue)
+{
+    int [,] result=new int[m,n];
+    for (int i=0;i<m;i++)
+    {
+       for (int j=0;j<n;j++) 
+       {
+        result[i,j]=new Random().Next(minValue, maxValue +1);
+       }
+    }
+    return result;
+}
+void PrintArray(int [,] inArray)
+{
+    for (int i=0;i<inArray.GetLength(0);i++)
+    {
+        for (int j=0;j<inArray.GetLength(0);j++)
+        {
+            Console.Write($"{inArray[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите кол-во стобцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, cols, 0, 100);
+PrintArray(array);
+Console.WriteLine();
+int [,] result=ChangeArray(array);
+PrintArray(result);
+
+
